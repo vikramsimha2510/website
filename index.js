f46a2a19fd8fb22661ceda906015449a41be7359
vikramsimha2510 <111378129+vikramsimha2510@users.noreply.github.com>
2022-08-16 21:56:54 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
+const app = express();
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"));
+})
+app.get("/style", (req, res) => {
+    res.sendFile(path.join(__dirname, "/style.css"));
+})
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/about.html"));
+})
+app.listen(3000, () => console.log("Started express server"));
